26d359c514d6c334ec240e788572ab5e2b9bc4f6
prosich <prosich@gmail.com>
2018-11-13 14:17:07 +0100

    Update Espini.cpp

--- Espini.cpp
+++ Espini.cpp
@@ -8,7 +8,7 @@ Espini::Espini(ESP8266WiFiMulti wifis,
 
   snprintf(chipId,sizeof(chipId),"%u",ESP.getChipId());
 
-  syslog=new Syslog(udp,otas,514,chipId,app,LOG_INFO);
+  syslog=new Syslog(udp,logs,514,chipId,app,LOG_INFO);
 
   WiFi.mode(WIFI_STA);
   Serial.println(WiFi.macAddress()+" conectando a wifi");
@@ -17,8 +17,8 @@ Espini::Espini(ESP8266WiFiMulti wifis,
     for (int segs=1; segs<=15; segs++) {
       if (wifis.run() == WL_CONNECTED) {
         Serial.println(WiFi.localIP()); 
-        ESPhttpUpdate.update("ota.rosich.es",80,
-          "/cgi/ota?ser="+String(chipId)+"&app="+app+"&ver="+ver);
+        ESPhttpUpdate.update(otas,80,
+          "/espota?ser="+String(chipId)+"&app="+app+"&ver="+ver);
 		return;
       }
       delay(1000);
